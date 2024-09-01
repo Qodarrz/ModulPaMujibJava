0d86de86aa623fb7262a8c675ada90c3f55b3522
Muhammad Heidar Arrizqie <137019596+Qodarrz@users.noreply.github.com>
2024-09-01 10:53:52 +0700

    NetBeansProjects

--- Bab11Soal2/src/main/java/com/mycompany/bab11soal2/Bab11Soal2Main.java
+++ Bab11Soal2/src/main/java/com/mycompany/bab11soal2/Bab11Soal2Main.java
@@ -15,37 +15,29 @@ public class Bab11Soal2Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Memasukkan data karyawan 1
         System.out.println("Masukkan data karyawan 1:");
         System.out.print("ID Karyawan: ");
         int id1 = scanner.nextInt();
-        scanner.nextLine();  // Mengonsumsi newline
-
+        scanner.nextLine();
         System.out.print("Nama Karyawan: ");
         String nama1 = scanner.nextLine();
 
         System.out.print("Golongan: ");
         String gol1 = scanner.nextLine();
 
-        // Membuat objek karyawan 1
         Bab11Soal2 karyawan1 = new Bab11Soal2(id1, nama1, gol1);
 
-        // Memasukkan data karyawan 2
         System.out.println("\nMasukkan data karyawan 2:");
         System.out.print("ID Karyawan: ");
         int id2 = scanner.nextInt();
-        scanner.nextLine();  // Mengonsumsi newline
-
+        scanner.nextLine();
         System.out.print("Nama Karyawan: ");
         String nama2 = scanner.nextLine();
 
         System.out.print("Golongan: ");
         String gol2 = scanner.nextLine();
-
-        // Membuat objek karyawan 2
         Bab11Soal2 karyawan2 = new Bab11Soal2(id2, nama2, gol2);
 
-        // Menampilkan informasi karyawan
         System.out.println("\nInformasi Karyawan 1:");
         karyawan1.tampilkanInfo();
 
